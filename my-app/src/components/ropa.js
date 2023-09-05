91ed28d35f21840acb3c4169ab9c3a10f9f7dadf
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-09-05 09:57:52 -0300

    get products

--- my-app/src/components/ropa.js
+++ my-app/src/components/ropa.js
@@ -1,79 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
+
 export default function Ropa() {
-  return (
-    <>
-      <div class="row mb-5">
-        <div
-          class="col-sm-6 col-lg-4 mb-4 aos-init aos-animate"
-          data-aos="fade-up"
-        >
-          <div class="block-4 text-center border">
-            <figure class="block-4-image">
-            <Link to="/Shop_Single">
-                <img
-                  src="images/cloth_1.jpg"
-                  alt="Image placeholder"
-                  class="img-fluid"
-                ></img>
-              </Link>
-            </figure>
-            <div class="block-4-text p-4">
-              <h3>
-                <Link to="/Shop_Single">Tank Top</Link>
-              </h3>
-              <p class="mb-0">Finding perfect t-shirt</p>
-              <p class="text-primary font-weight-bold">$50</p>
+  const [products, setProducts] = useState([]);
+
+    axios.get('https://dummyjson.com/products')
+      .then(function (response) {
+        setProducts(response.data.products); 
+        console.log(response.data.products);
+      })
+      .catch(function (error) {
+        console.error("Error fetching data:", error);
+      });
+      console.log(products);
+      if (products==null) {
+       return <div>Loading...</div>
+      }else{
+        return (
+   
+          <>
+          
+            <div className="row mb-5">
+              {products.map((element) => (
+                <div
+                  key={element.id} 
+                  className="col-sm-6 col-lg-4 mb-4 aos-init aos-animate"
+                  data-aos="fade-up"
+                >
+                  <div className="block-4 text-center border">
+                    <figure className="block-4-image">
+                      <Link to="/Shop_Single">
+                        <img
+                          src={element.thumbnail}
+                          alt="placeholder"
+                          className="img-fluid"
+                        />
+                      </Link>
+                    </figure>
+                    <div className="block-4-text p-4">
+                      <h3>
+                        <Link to="/Shop_Single">{element.title}</Link>
+                      </h3>
+                      <p className="mb-0">Finding the perfect t-shirt</p>
+                      <p className="text-primary font-weight-bold">{element.Price}</p>
+                    </div>
+                  </div>
+                </div>
+              ))}
             </div>
-          </div>
-        </div>
-        <div
-          class="col-sm-6 col-lg-4 mb-4 aos-init aos-animate"
-          data-aos="fade-up"
-        >
-          <div class="block-4 text-center border">
-            <figure class="block-4-image">
-            <Link to="/Shop_Single">
-                <img
-                  src="images/shoe_1.jpg"
-                  alt="Image placeholder"
-                  class="img-fluid"
-                ></img>
-              </Link>
-            </figure>
-            <div class="block-4-text p-4">
-              <h3>
-              <Link to="/Shop_Single">Corater</Link>
-              </h3>
-              <p class="mb-0">Finding perfect products</p>
-              <p class="text-primary font-weight-bold">$50</p>
-            </div>
-          </div>
-        </div>
-        <div
-          class="col-sm-6 col-lg-4 mb-4 aos-init aos-animate"
-          data-aos="fade-up"
-        >
-          <div class="block-4 text-center border">
-            <figure class="block-4-image">
-            <Link to="/Shop_Single">
-                <img
-                  src={"images/cloth_2.jpg"}
-                  alt="Image placeholder"
-                  class="img-fluid"
-                ></img>
-              </Link>
-            </figure>
-            <div class="block-4-text p-4">
-              <h3>
-              <Link to="/Shop_Single">Polo Shirt</Link>
-              </h3>
-              <p class="mb-0">Finding perfect products</p>
-              <p class="text-primary font-weight-bold">$50</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
-}
+          </>
+        );
+      }
+      
+      }
