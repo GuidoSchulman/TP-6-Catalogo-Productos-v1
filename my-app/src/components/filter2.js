aeca73b26159a36f9e0d877257b86725df135d21
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-09-07 09:03:25 -0300

    filter

--- my-app/src/components/filter2.js
+++ my-app/src/components/filter2.js
@@ -1,58 +1,55 @@
-import React from 'react'
+import React, { useState, useEffect, createContext, useContext } from "react";
+import axios from 'axios';
 
-export default function filter2() {
-  return (
-    <>
-    
-          <div class="border p-4 rounded mb-4">
-            <h3 class="mb-3 h6 text-uppercase text-black d-block">Categories</h3>
-            <ul class="list-unstyled mb-0">
-              <li class="mb-1"><a href="#" class="d-flex"><span>Men</span> <span class="text-black ml-auto">(2,220)</span></a></li>
-              <li class="mb-1"><a href="#" class="d-flex"><span>Women</span> <span class="text-black ml-auto">(2,550)</span></a></li>
-              <li class="mb-1"><a href="#" class="d-flex"><span>Children</span> <span class="text-black ml-auto">(2,124)</span></a></li>
-            </ul>
-          </div>
+// Create a context for managing the selected category
+const CategoriaContext = createContext();
 
-          <div class="border p-4 rounded mb-4">
-            <div class="mb-4">
-              <h3 class="mb-3 h6 text-uppercase text-black d-block">Filter by Price</h3>
-              <div id="slider-range" class="border-primary ui-slider ui-corner-all ui-slider-horizontal ui-widget ui-widget-content"><div class="ui-slider-range ui-corner-all ui-widget-header" style={{left: '15%', width: '45%'}}></div><span tabindex="0" class="ui-slider-handle ui-corner-all ui-state-default" style={{left: '15%'}}></span><span tabindex="0" class="ui-slider-handle ui-corner-all ui-state-default" style={{left: '60%'}}></span></div>
-              <input type="text" name="text" id="amount" class="form-control border-0 pl-0 bg-white" disabled=""/>
+export default function Filter2() {
+  const [filters, setFilters] = useState([]);
+  const [selectedCategoria, setSelectedCategoria] = useState(null);
+
+  useEffect(() => {
+    axios.get('https://dummyjson.com/products/categories')
+      .then(function (response) {
+        setFilters(response.data);
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
+  const SeleccionarCategoria = (element) => {
+    setSelectedCategoria(element);
+    console.log(element);
+  };
+
+  if (filters === null) {
+    return <div>Loading...</div>
+  } else {
+    return (
+      <CategoriaContext.Provider value={selectedCategoria}>
+        <div className="border p-4 rounded mb-4">
+          <h3 className="mb-3 h6 text-uppercase text-black d-block">Categories</h3>
+          {filters.map((element) => (
+            <div key={element}>
+              <ul className="list-unstyled mb-0">
+                <li className="mb-1">
+                  <a
+                    href="#"
+                    className="d-flex"
+                    onClick={() => SeleccionarCategoria(element)}
+                  >
+                    <span>{element}</span> <span className="text-black ml-auto"></span>
+                  </a>
+                </li>
+              </ul>
             </div>
-
-            <div class="mb-4">
-              <h3 class="mb-3 h6 text-uppercase text-black d-block">Size</h3>
-              <label for="s_sm" class="d-flex">
-                <input type="checkbox" id="s_sm" class="mr-2 mt-1"/> <span class="text-black">Small (2,319)</span>
-              </label>
-              <label for="s_md" class="d-flex">
-                <input type="checkbox" id="s_md" class="mr-2 mt-1"/> <span class="text-black">Medium (1,282)</span>
-              </label>
-              <label for="s_lg" class="d-flex">
-                <input type="checkbox" id="s_lg" class="mr-2 mt-1"/> <span class="text-black">Large (1,392)</span>
-              </label>
-            </div>
-
-            <div class="mb-4">
-              <h3 class="mb-3 h6 text-uppercase text-black d-block">Color</h3>
-              <a href="#" class="d-flex color-item align-items-center">
-                <span class="bg-danger color d-inline-block rounded-circle mr-2"></span> <span class="text-black">Red (2,429)</span>
-              </a>
-              <a href="#" class="d-flex color-item align-items-center">
-                <span class="bg-success color d-inline-block rounded-circle mr-2"></span> <span class="text-black">Green (2,298)</span>
-              </a>
-              <a href="#" class="d-flex color-item align-items-center">
-                <span class="bg-info color d-inline-block rounded-circle mr-2"></span> <span class="text-black">Blue (1,075)</span>
-              </a>
-              <a href="#" class="d-flex color-item align-items-center">
-                <span class="bg-primary color d-inline-block rounded-circle mr-2"></span> <span class="text-black">Purple (1,075)</span>
-              </a>
-            </div>
-
-          </div>
-        
-
-    
-    </>
-  )
+          ))}
+        </div>
+      </CategoriaContext.Provider>
+    );
+  }
 }
+console.log(CategoriaContext);
+export { CategoriaContext };
--- my-app/src/components/ropa.js
+++ my-app/src/components/ropa.js
@@ -1,56 +1,73 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, createContext, useContext } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
+import { CategoriaContext } from "./filter2";
 
 export default function Ropa() {
   const [products, setProducts] = useState([]);
-
-    axios.get('https://dummyjson.com/products')
+  const selectedCategory = useContext(CategoriaContext);
+  console.log(selectedCategory);
+  if (selectedCategory != null) {
+    products.length = 0;
+    axios
+      .get("https://dummyjson.com/products/category/" + selectedCategory)
       .then(function (response) {
-        setProducts(response.data.products); 
+        setProducts(response.data.products);
         console.log(response.data.products);
-      })
+      }, [])
       .catch(function (error) {
         console.error("Error fetching data:", error);
       });
-      console.log(products);
-      if (products==null) {
-       return <div>Loading...</div>
-      }else{
-        return (
-   
-          <>
-          
-            <div className="row mb-5">
-              {products.map((element) => (
-                <div
-                  key={element.id} 
-                  className="col-sm-6 col-lg-4 mb-4 aos-init aos-animate"
-                  data-aos="fade-up"
-                >
-                  <div className="block-4 text-center border">
-                    <figure className="block-4-image">
-                      <Link to="/Shop_Single">
-                        <img
-                          src={element.thumbnail}
-                          alt="placeholder"
-                          className="img-fluid"
-                        />
-                      </Link>
-                    </figure>
-                    <div className="block-4-text p-4">
-                      <h3>
-                        <Link to="/Shop_Single">{element.title}</Link>
-                      </h3>
-                      <p className="mb-0">Finding the perfect t-shirt</p>
-                      <p className="text-primary font-weight-bold">{element.Price}</p>
-                    </div>
-                  </div>
+    
+  } else {
+    axios
+      .get("https://dummyjson.com/products")
+      .then(function (response) {
+        setProducts(response.data.products);
+        console.log(response.data.products);
+      }, [])
+      .catch(function (error) {
+        console.error("Error fetching data:", error);
+      });
+  }
+
+  console.log(products);
+  if (products == null) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <>
+        <div className="row mb-5" id="todo">
+          {products.map((element) => (
+            <div
+              key={element.id}
+              className="col-sm-6 col-lg-4 mb-4 aos-init aos-animate"
+              data-aos="fade-up"
+            >
+              <div className="block-4 text-center border">
+                <figure className="block-4-image">
+                  <Link to="/Shop_Single">
+                    <img
+                      src={element.thumbnail}
+                      alt="placeholder"
+                      className="img-fluid"
+                    />
+                  </Link>
+                </figure>
+                <div className="block-4-text p-4">
+                  <h3>
+                    <Link to="/Shop_Single">{element.title}</Link>
+                  </h3>
+                  <p className="mb-0">{element.description}</p>
+                  <p className="text-primary font-weight-bold">
+                    {element.Price}
+                  </p>
                 </div>
-              ))}
+              </div>
             </div>
-          </>
-        );
-      }
-      
-      }
+          ))}
+        </div>
+      </>
+    );
+  }
+}
