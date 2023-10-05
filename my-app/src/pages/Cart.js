3b9607ce19b5c12e7ace7349523f2c403106b86e
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-10-05 10:31:17 -0300

    carrito

--- my-app/src/pages/Cart.js
+++ my-app/src/pages/Cart.js
@@ -2,6 +2,7 @@ import Header from "../components/header.js";
 import Tabla from "../components/tabla.js";
 
 import Recuento from "../components/recuento.js";
+import { Link } from "react-router-dom";
 function Cart(props) {
   return (
     <body>
@@ -17,8 +18,9 @@ function Cart(props) {
                 </button>
               </div>
               <div className="col-md-6">
-                <button className="btn btn-outline-primary btn-sm btn-block">
-                  Continue Shopping
+                
+                <button className="btn btn-outline-primary btn-sm btn-block" >
+                <Link to="/shop">Continue Shopping</Link>
                 </button>
               </div>
             </div>
